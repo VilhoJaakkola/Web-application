4fa5d67be04a6e8144d45a046341302c2694efd7
Vilho <vilhoejjaakkola@gmail.com>
2026-01-01 15:18:14 +0200

    Finished fetching data and showing it on front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,12 @@
-import { useState } from 'react'
-
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './utils/trpc'
+import UserList from './components/user-list'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <UserList />
+    </QueryClientProvider>
   )
 }
 
---
+++ frontend/src/components/user-list.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { trpc } from "../utils/trpc";
+
+export default function UserList() {
+  const { data: users, isLoading, error } = useQuery(trpc.users.getAll.queryOptions());
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <h2>Users</h2>
+      <ul>
+        {users?.map(user => (
+          <li key={user.id}>
+            {user.first_name} {user.last_name} - {user.email} 
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
